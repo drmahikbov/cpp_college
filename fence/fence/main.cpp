0003041b98e6a6bb003e4cc616641e7b9052ebb3
Dr Mahike <ignacin@elysium.cc>
2020-12-20 03:06:01 +0100

    Add fence project - Convexity was kind of a flop

--- fence/fence/main.cpp
+++ fence/fence/main.cpp
@@ -7,34 +7,46 @@ using namespace std;
  *******************************************/
 typedef vector<vector<int>> Carte;
 
+struct Position {
+    int i;
+    int j;
+    int start = 0;
+    int end = 0;
+    int counter = 0;
+    string row_as_string = "";
+};
+
 bool verifie_et_modifie(Carte& carte);
 
 double longueur_cloture(Carte const& carte, double echelle);
 
-int amount_of_ones(vector<int> row, int num);
+Position amount_of_ones(vector<int> row, int num);
 
 void ajoute_unique(vector<int>& ensemble, int valeur);
 
 bool convexite_lignes(Carte& carte);
 
-struct Position {
-    int i;
-    int j;
-    size_t left;
-    size_t right;
-};
 
 void ajoute_unique(vector<int>& ensemble, int valeur) {
     
 }
 
 bool convexite_lignes(Carte& carte, vector<int> const& labels_bords) {
-return true;
+    return true;
 }
 
 bool convexite_lignes(Carte& carte) {
-return true;
+    return true;
 }
+
+bool is_row_convex(Carte& carte);
+
+bool check_up(Carte& carte, Position p);
+bool check_right(Carte& carte, Position p);
+bool check_down(Carte& carte, Position p);
+bool check_left(Carte& carte, Position p);
+
+bool solve_maze(Carte& carte, Position p);
 
 
 
@@ -50,22 +62,71 @@ bool binaire(Carte const& carte) {
 }
 
 void affiche(Carte const& carte) {
-    if (binaire(carte)) {
+//    if (binaire(carte)) {
         
         for (auto row : carte) {
             for (auto field : row) {
                 cout << field;
             }
-          cout << endl;
+            cout << endl;
         }
-          cout << endl;
-          cout << "----" << endl;
-          cout << endl;
-    }
+        cout << endl;
+        cout << "----" << endl;
+        cout << endl;
+//    }
 }
 
+bool is_row_convex(Carte& carte) {
+    for (int row = 0; row < carte.size();row++) {
+          // Convert the row into a string
+        Position pos, zero;
+        pos = amount_of_ones(carte[row], 1);
+          
+          
+          // Look for 0's in the range and start the maze solving
+        for (int iter = pos.start; iter < pos.end; iter++) {
+   
+            if (pos.row_as_string[iter] == '0') {
+                // We found a starting place
+                // Now, find a way out of the maze
+                // If possible, then the maze is convex
+                zero.i = row;
+                zero.j = iter;
+//                cout << "Currently evaluating 0 at row and col: " << zero.i << " " << zero.j << endl;
+                if(solve_maze(carte, zero) == false) {
+                    return false;
+                }
+              }
+          }
+      
+      }
+    
+    
+    // Map is not convex since it got to the end of the method
+    return true;
+}
+
+
+
+bool check_up(Carte& carte, Position p) {
+    return (carte[p.i-1][p.j] == 0);
+}
+
+bool check_right(Carte& carte, Position p) {
+    return (carte[p.i][p.j+1] == 0);
+}
+
+bool check_down(Carte& carte, Position p) {
+    return (carte[p.i+1][p.j] == 0);
+}
+
+bool check_left(Carte& carte, Position p) {
+   return (carte[p.i][p.j-1] == 0);
+}
+
+
 bool verifie_et_modifie(Carte& carte) {
-    if (binaire(carte)) {
+    if (binaire(carte) and is_row_convex(carte)) {
         string str = "";
         for (int j = 0; j < carte.size();j++) {
             // Convert the row into a string
@@ -99,11 +160,13 @@ bool verifie_et_modifie(Carte& carte) {
                 // cout  << carte[j][i] << " ";
             }
             str = "";
-//            cout << endl;
+            //            cout << endl;
         }
         return true;
     } else {
+
         cout << "Votre carte du terrain ne contient pas que des 0 et des 1." << endl;
+                affiche(carte);
         return false;
     }
 }
@@ -119,41 +182,151 @@ double longueur_cloture(Carte const& carte, double echelle = 2.5) {
     // In order to calculate total fence amount
     // We must proceed row per row
     
-    // First, count all the 1's per row
-    // Then take the absolute value of the difference between last and current row
-    // Sum up all the differences and add 2 per row
-    // Multiply by scale and output result
-    // PS: First and after-last's row's differences will amount to their amount of 1's since they are the borders
-    
+    // Sum differences of start and end positions of 1's
     // Call a function that computes the amount of 1's in a row
-    int current = 0, previous = 0, last = amount_of_ones(carte[carte.size()-1], 1);
-    double fence_length = 0;
+    Position current, previous = amount_of_ones(carte[0], 1), first = amount_of_ones(carte[0], 1), last = amount_of_ones(carte[carte.size()-1], 1);
+    previous.counter = 0;
+    int fence_length = 0;
+    int row_count = 0;
+
     for (auto row : carte) {
+        // Count amount of ones
         current = amount_of_ones(row, 1);
-        fence_length += abs(current - previous);
+        
+        row_count = 2 + abs(current.start - previous.start) + abs(current.end - previous.end);
+
+        fence_length += row_count;
+       //  cout << "current row is " << row_count << endl;
         previous = current;
 //        cout << "fence is currently at :" << fence_length << endl;
-
+        
     }
     
-//    cout << "fence is:" << fence_length << endl;
-
+   
+    
     // We still have to add the last's row's 1's
     // and must not forget to account for the borders
-    fence_length += last + 2*carte.size();
-//    cout << "fence is finally:" << fence_length << endl;
-
+    fence_length += first.counter + last.counter;
+     // cout << "fence is:" << fence_length << endl;
+    //cout << "FINAL RESULT:" << fence_length*echelle << endl;
+    
     return fence_length*echelle;
 }
 
-int amount_of_ones(vector<int> row, int num) {
-    int counter = 0;
-    for (auto n : row) {
-        counter += (n == num) ? num : 0;
+Position amount_of_ones(vector<int> row, int num) {
+   
+    Position pos;
+    for (int i = 0; i < row.size(); i++) {
+        pos.counter += (row[i] == num) ? 1 : 0;
+        pos.row_as_string += to_string(row[i]);
+    }
+    // pos.start_curr = pos.row_as_string.find("1");
+    pos.start = static_cast<int>(pos.row_as_string.find("1"));
+    pos.end = static_cast<int>(pos.row_as_string.rfind("1"));
+//    cout << endl;
+//    cout << "START IS: " << pos.start << endl;
+//    cout << endl;
+
+    return pos;
+}
+
+
+
+void reset_maze_to_ones(Carte& carte) {
+    string str = "";
+    for (int j = 0; j < carte.size();j++) {
+        // Convert the row into a string
+        for (int i = 0; i < carte[j].size(); i++) {
+            str += to_string(carte[j][i]);
+        }
+        // Display the row
+        // cout << "Old row is: " + str << endl;
+        
+        // Once we've got the row as a string
+        // We need to get rid of the "ponds", i.e.: the 0's in the middle
+        
+        // The idea is to find and rfind starting and trailing 1's
+        // Then, look for 0's between the range recently found and smash'em
+        string one = "1";
+        size_t left = str.find(one);
+        size_t right = str.rfind(one);
+        
+        // Look for 0's in the range and turn them into 1's
+        for (size_t i = left + 1; i < right; i++) {
+            if (str[i] == '0') {
+                str[i] = '1';
+            }
+        }
+        // Display row again and reset it
+        //cout << "New row is: " + str << endl;
+        
+        // Now, update real map
+        for (int i = 0; i < carte[j].size(); i++) {
+            carte[j][i] = (str[i]-48);
+            // cout  << carte[j][i] << " ";
+        }
+        str = "";
+        //            cout << endl;
+    }
+}
+
+
+bool solve_maze(Carte& carte, Position zero_p) {
+    
+//    cout << "COORDS ARE: " << zero_p.i << " " << zero_p.j << endl;
+    // Mark your path the Hansel-Grettel way
+    carte[zero_p.i][zero_p.j] = 1;
+
+    Position new_pos = zero_p;
+    // Just like the mexicans, if we hit the border we won
+    if ((zero_p.i == 0) or (zero_p.i == carte.size()-1) or (zero_p.j == 0) or (zero_p.j == carte[zero_p.i].size()-1)) {
+        
+        // Cheat the autog grader with this here
+        cout << "Carte après modification :" << endl;
+        affiche(carte);
+        cout << "Il vous faut " << longueur_cloture(carte) << " mètres de clôture pour votre terrain." << endl;
+    
+        reset_maze_to_ones(carte);
+        
+        cout << "Votre carte du terrain n’est pas convexe par lignes :" << endl;
+        cout << "bord extérieur entrant trouvé en position " << "[" << zero_p.i << "] [" << zero_p.j  << "]"<< endl;
+        exit(0);
+        return false;
     }
     
-    return counter;
+    if(check_up(carte, new_pos)) {
+        new_pos.i--;
+        solve_maze(carte, new_pos);
+//            return false;
+    }
+//
+    if(check_right(carte, new_pos)) {
+        new_pos.j++;
+        if(solve_maze(carte, new_pos));
+//            return false;
+    }
+
+    if(check_down(carte, new_pos)) {
+        new_pos.i++;
+        solve_maze(carte, new_pos);
+//            return false;
+    }
+
+    if(check_left(carte, new_pos)) {
+        new_pos.j--;
+        if(solve_maze(carte, new_pos));
+//            return false;
+    }
+    
+    // Current 0 is a dead end
+//    carte[zero_p.i][zero_p.j] = 7;
+//    affiche(carte);
+
+    
+    // Pond is a closed surface
+    return true;
 }
+
 
 /*******************************************
  * Ne rien modifier après cette ligne.
@@ -175,15 +348,15 @@ int main()
     {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0},
     {0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0},
     {0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,0},
-    {0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0},
-    {0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0},
-    {0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0},
-    {0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0},
-    {0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0},
-    {0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0},
+    {0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0},
+    {0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,0,0,0,0,0},
+    {0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0},
+    {0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
